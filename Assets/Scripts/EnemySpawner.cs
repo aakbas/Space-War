1541748fa8f5f285c136f96ed2428a6384004170
Abdullah Akbaş <39574496+aakbas@users.noreply.github.com>
2020-09-21 20:09:05 +0300

    wave spawning

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private  IEnumerator SpawnAllEnemiesInWave(WaveConfig currentWave)
+    {
+        for (int i = 0; i < currentWave.GetNumberOfEnemies(); i++)
+        {
+            Instantiate(
+          currentWave.GetEnemyPrefab(),
+          currentWave.GetWaypoints()[0].transform.position,
+          Quaternion.identity
+          );
+            yield return new WaitForSeconds(currentWave.GetTimeBetweenSpawns());
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
